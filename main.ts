9172e08691a9ebd99f4930b6e41b20ca436c5da7
iontitan <102923447+iontitan@users.noreply.github.com>
2022-10-22 14:09:51 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.showString("Hello!")
+basic.pause(100)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.forever(function () {
+	
+})
